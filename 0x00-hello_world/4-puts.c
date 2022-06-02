6cec193703fc5e264322ff54bfca29935f3c345f
Barack <walindibarack@yahoo.com>
2022-06-02 09:47:24 -0700

    Task4- Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * run starts at main
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multingual puzzle");
+	return (0);
+}
