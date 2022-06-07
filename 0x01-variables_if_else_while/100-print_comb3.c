8c2d3029e52ad1fca3aaf2842074ac7c2fd3ed59
Barack <walindibarack@yahoo.com>
2022-06-06 23:44:24 -0700

    Two-digit combos

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -21,7 +21,7 @@ int main(void)
 			putchar((digit2 % 10) + '0');
 
 			/*don't add ',' and space when at 89*/
- 			if (digit1 == 8 && digit2 == 9)
+			if (digit1 == 8 && digit2 == 9)
 				continue;
 
 			putchar(',');
