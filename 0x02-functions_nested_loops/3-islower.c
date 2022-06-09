90f6cdc83196ab122881e6a78920d2f91ce60a6f
Barack <walindibarack@yahoo.com>
2022-06-08 17:56:38 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,24 @@
+/**
+ * Author: Barack Walindi
+ */
+
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase character
+ *
+ * Return: 1 if lowercase, 0 otherwise
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
+}
