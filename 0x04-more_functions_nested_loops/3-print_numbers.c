fd3bd4e6a55f10964c4aa97f03e254ec9d8f4c39
Barack <walindibarack@yahoo.com>
2022-06-16 00:45:21 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers 0 to 9
+ * followed by a new line
+ *
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int x;
+	
+	for (c = '0'; c <= 9; c++)
+	{
+		_putchar(c);
+	}
+
+	_putchar('\n');
+}
