6eb2652f2089c6a92d249daa4694d854e932fa1e
Barack <walindibarack@yahoo.com>
2022-06-15 20:00:48 -0700

    natural numbers

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,42 @@
+#imclude "main.h"
+
+/**
+ * print_to_98 - prints all natural numbers from n to 98
+ * followed by a new line
+ *
+ * @n: first integer
+ *
+ * Return: void
+ */
+
+void print_to_98(int n)
+{
+	int i = 0;
+
+	if (n < 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			if (i != n)
+			{
+				printf(",");
+			}
+
+			printf(%d, i);
+		}
+	}
+
+	else
+	{
+		for (i = n; i <= 98; i++)
+		{
+			if (i != n)
+			{
+				printf(",");
+			}
+
+			printf(%d, i);
+		}
+	}
+	putchar('\n');
+}
