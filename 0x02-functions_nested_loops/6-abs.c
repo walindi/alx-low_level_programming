009532a022faf061933f66c9e27135b838cd6a72
Barack <walindibarack@yahoo.com>
2022-06-15 18:38:07 -0700

    Absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _abs - returns absolute value of an int
+ *
+ * @r: integer from which to get absolute value
+ *
+ * Return: Always 0
+ */
+
+int _abs(int)
+{
+	int r;
+
+	if (r < 0)
+	{
+		return (n * -1);
+	}
+
+	return (0);
+}
